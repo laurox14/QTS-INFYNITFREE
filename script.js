43ea4bf354d8efeb09b00e418e64c67e63a0a78f
laurox14 <laurodominguessilvaneto@gmail.com>
2026-05-31 11:23:54 -0300

    todos exercicios resolvidos

--- script.js
+++ script.js
@@ -6,12 +6,13 @@ function mensagemBoasVindas(nome) {
   return `Olá, ${nome}!`;
 }
 
-
+// exercicio 1
 function qualMaior(x , y){
 if(x>y){return x}
 else{return y}
 }
 
+// exercicio 2
 function parImpar(x,y){
   
   if(x%2==0){
@@ -27,19 +28,76 @@ function parImpar(x,y){
 
 }
 
+// exercicio 3
 function podeVotar(x){
   if(x>18 && x<70){{return 'Você é obrigado a votar'}}
   if(x>16 && x<18){{return 'Você não é obrigado a votar'}}
-  if(x>70){{return 'Você não é obrigado a votar'}}
-  if(x<16){{return 'Você não pode votar'}}
+  if(x>=70){{return 'Você não é obrigado a votar'}}
+  if(x<=16){{return 'Você não pode votar'}}
 }
 
+// exercicio 4
 function negPos(x){
   if(x>0){return `o numero ${x} é positivo`}
   if(x<0){return `o numero ${x} é negativo`}
   if(x==0){return `o numero ${x} é igual a 0`}
 }
 
-module.exports = { somar, mensagemBoasVindas, qualMaior, parImpar, podeVotar,negPos};
+// exercicio 5
+function salBonus(x){
+  if(x<2000)
+    return (x * 0.2)
+}
+  if(x>=2000 && x< 5000){
+    return (x * 0.1)
+}
+  if(x>=5000){
+    return (x * 0.05)
+}
+
+// exercicio 6
+function dirigir(idade){
+  if(idade<18){return 'Você não pode dirigir'}
+  else{return 'Você pode dirigir'}
+}
+
+// exercicio 7
+function mes(mes){
+  if(mes=='janeiro' || mes=='fevereiro' || mes=='março' || mes=='abril' || mes=='maio' || mes=='junho'){
+    return `${mes} é do primeiro semestre`
+  }
+  if(mes=='julho' || mes=='agosto' || mes=='setembro' || mes=='outubro' || mes=='novembro' || mes=='dezembro'){
+    return `${mes} é do segundo semestre`
+  }
+  return 'mes invalido'
+}
+
+// exercicio 8
+function conVogal(letra){
+  if(letra=='a' || letra=='e' || letra=='i' || letra=='o' || letra=='u'){
+    return `a letra ${letra} é vogal`
+  }
+  return`a letra ${letra} é consoante`
+}
+
+// exercicio 9
+function mediaArray(valores){
+  let soma = 0 
+  for (let i = 0; i < valores.length; i++) { 
+    soma += valores[i]
+  }
+  return (soma/valores.length)
+}
+
+// exercicio 10
+function acesso(nome,senha){
+  if(nome=='Admin' && senha == 123){
+    return 'Conectado com sucesso!'}
+  return 'Acesso negado! Usuário ou senha incorretos!'
+
+}
+
+
+module.exports = { somar, mensagemBoasVindas, qualMaior, parImpar, podeVotar,negPos,salBonus,dirigir, mes, conVogal, mediaArray, acesso};
 // export default { somar, mensagemBoasVindas };
 
--- script.test.js
+++ script.test.js
@@ -1,24 +1,62 @@
-const { somar, mensagemBoasVindas, qualMaior, parImpar, podeVotar, negPos} = require('./script');
+const { somar, mensagemBoasVindas, qualMaior, parImpar, podeVotar, negPos, salBonus, dirigir, mes, conVogal, mediaArray, acesso} = require('./script');
 
+// exercicio 
 test('soma dois números', () => {
   expect(somar(2, 3)).toBe(5);
 });
 
+// exercicio 
 test('mensagem de boas vindas', () => {
   expect(mensagemBoasVindas('Diogo')).toBe('Olá, Diogo!');
 });
 
+// exercicio 1
 test('retorna o maior', () => {
-  expect(qualMaior(10,20)).toBe(20);
+  expect(qualMaior(10, 20)).toBe(20);
 });
 
+// exercicio 2
 test('retorna qual é par e qual é impar', () => {
-  expect(parImpar(2,3)).toBe('o primeiro numero 2 é par', 'o segundo numero 3 é impar');
+  expect(parImpar(2, 3)).toBe('o primeiro numero 2 é par', 'o segundo numero 3 é impar');
 });
 
+// exercicio 3
 test('retorna se a pessoa pode votar , voto opcional ou não pode votar.', () => {
   expect(podeVotar(17)).toBe('Você não é obrigado a votar');
 });
+
+// exercicio 4
 test('retorna se é positivo, negativo ou igual a zero.', () => {
   expect(negPos(17)).toBe('o numero 17 é positivo');
 });
+
+// exercicio 5
+test('recebe o salário e retorna o bônus.', () => {
+  expect(salBonus(4000)).toBe(400);
+});
+
+// exercicio 6
+test('exibe “pode dirigir” ou “não pode dirigir”', () => {
+  expect(dirigir(18)).toBe('Você pode dirigir');
+});
+
+// exercicio 7
+test('exibe se o mês é do primeiro semestre ou segundo semestre.', () => {
+  expect(mes("março")).toBe('março é do primeiro semestre');
+});
+
+// exercicio 8
+test('verifica se a letra é vogal ou consoante', () => {
+  expect(conVogal("a")).toBe('a letra a é vogal');
+});
+
+// exercicio 9
+test('calcula a média dos valores do array', () => {
+  expect(mediaArray([10, 20, 30, 40, 50])).toBe(30);
+});
+
+// exercicio 10
+test('verifica acesso válido', () => {
+  expect(acesso('Admin', 123)).toBe('Conectado com sucesso!');
+});
+
